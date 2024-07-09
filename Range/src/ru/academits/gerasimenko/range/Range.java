ea3a52a411cd661e8d121de35349475adf746126
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-07-09 15:37:13 +0700

    Add solution for Range

--- Range/src/ru/academits/gerasimenko/range/Range.java
+++ Range/src/ru/academits/gerasimenko/range/Range.java
@@ -1,4 +1,103 @@
 package ru.academits.gerasimenko.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
+
+    public Range getIntersection(Range range) {
+        if (from >= range.to || to <= range.from) {
+            return null;
+        }
+
+        if (from >= range.from && to <= range.to) {
+            return this;
+        }
+
+        if (from <= range.from && to >= range.to) {
+            return range;
+        }
+
+        if (from < range.from && to < range.to) {
+            return new Range(range.from, to);
+        }
+
+        return new Range(from, range.to);
+    }
+
+    public Range[] getUnion(Range range) {
+        if (to < range.from) {
+            return new Range[]{this, range};
+        }
+
+        if (from > range.to) {
+            return new Range[]{range, this};
+        }
+
+        if (from <= range.from && to >= range.to) {
+            return new Range[]{this};
+        }
+
+        if (from >= range.from && to <= range.to) {
+            return new Range[]{range};
+        }
+
+        if (from < range.from) {
+            return new Range[]{new Range(from, range.to)};
+        }
+
+        return new Range[]{new Range(range.from, to)};
+    }
+
+    public Range[] getDifference(Range range) {
+        if (from >= range.from && to <= range.to) {
+            return null;
+        }
+
+        if (from >= range.to || to <= range.from) {
+            return new Range[]{this};
+        }
+
+        if (from < range.from && to < range.to) {
+            return new Range[]{new Range(from, range.from)};
+        }
+
+        if (from > range.from && to > range.to) {
+            return new Range[]{new Range(range.to, to)};
+        }
+
+        return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+    }
+
+    public static void print(Range range) {
+        System.out.printf("[%f; %f]%n", range.from, range.to);
+    }
 }
--- Range/src/ru/academits/gerasimenko/range/main/Main.java
+++ Range/src/ru/academits/gerasimenko/range/main/Main.java
@@ -1,4 +1,97 @@
 package ru.academits.gerasimenko.range.main;
 
+import ru.academits.gerasimenko.range.Range;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Часть 1.");
+
+        Range range1 = new Range(-100, 0);
+
+        System.out.print("Заданный диапазон: ");
+        Range.print(range1);
+
+        System.out.println("Длина диапазона: " + range1.getLength());
+        System.out.println();
+
+        System.out.println("Доступ к полям объекта при помощи геттеров:");
+        System.out.println("Начало диапазона: " + range1.getFrom());
+        System.out.println("Конец диапазона: " + range1.getTo());
+
+        System.out.println();
+
+        range1.setFrom(1.4);
+        range1.setTo(25.7);
+
+        System.out.print("Диапазон после изменения границ: ");
+        Range.print(range1);
+
+        System.out.println("Длина диапазона: " + range1.getLength());
+        System.out.println();
+
+        double number = 123;
+
+        if (range1.isInside(number)) {
+            System.out.printf("Число %f принадлежит диапазону ", number);
+        } else {
+            System.out.printf("Число %f не принадлежит диапазону ", number);
+        }
+
+        Range.print(range1);
+        System.out.println();
+
+        System.out.println("Часть 2.");
+
+        System.out.print("Первый диапазон: ");
+        Range.print(range1);
+
+        Range range2 = new Range(14, 17);
+
+        System.out.print("Второй диапазон: ");
+        Range.print(range2);
+
+        System.out.println();
+
+        Range intersectionRange = range1.getIntersection(range2);
+
+        if (intersectionRange == null) {
+            System.out.println("Диапазоны не пересекаются.");
+        } else {
+            System.out.println("Пересечение:");
+            Range.print(intersectionRange);
+        }
+
+        System.out.println();
+
+        Range[] unionRanges = range1.getUnion(range2);
+
+        if (unionRanges.length == 1) {
+            System.out.println("Диапазон объединения:");
+        } else {
+            System.out.println("Диапазоны объединения:");
+        }
+
+        for (Range range : unionRanges) {
+            Range.print(range);
+        }
+
+        System.out.println();
+
+        Range[] differenceRanges = range1.getDifference(range2);
+
+        if (differenceRanges == null) {
+            System.out.println("Разность указанных диапазонов является пустым множеством.");
+            return;
+        }
+
+        if (differenceRanges.length == 1) {
+            System.out.println("Диапазон разности:");
+        } else {
+            System.out.println("Диапазоны разности:");
+        }
+
+        for (Range range : differenceRanges) {
+            Range.print(range);
+        }
+    }
 }
