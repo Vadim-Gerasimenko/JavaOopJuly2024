91f5fa2d5122443c386e7f3358d90bb0cbe0c52b
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-07-10 17:35:01 +0700

    Implemented most of the methods in Matrix

--- Vector/src/ru/academits/gerasimenko/vector/Matrix.java
+++ Vector/src/ru/academits/gerasimenko/vector/Matrix.java
@@ -1,2 +1,193 @@
-package ru.academits.gerasimenko.vector;public class Matrix {
+package ru.academits.gerasimenko.vector;
+
+public class Matrix {
+    private Vector[] rows;
+
+    public Matrix(int n, int m) {
+        if (n <= 0 || m <= 0) {
+            throw new IllegalArgumentException("The sizes of the matrix must be positive.");
+        }
+
+        rows = new Vector[n];
+
+        for (int i = 0; i < n; i++) {
+            rows[i] = new Vector(m);
+        }
+    }
+
+    public Matrix(Matrix matrixToCopy) {
+        int matrixToCopyRowsCount = matrixToCopy.getRowsCount();
+        rows = new Vector[matrixToCopyRowsCount];
+        for (int i = 0; i < matrixToCopyRowsCount; i++) {
+            rows[i] = new Vector(matrixToCopy.rows[i]);
+        }
+    }
+
+    public Matrix(double[][] array) {
+        rows = new Vector[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            rows[i] = new Vector(array[i]);
+        }
+    }
+
+    public Matrix(Vector[] vectorsArray) {
+        rows = new Vector[vectorsArray.length];
+
+        for (int i = 0; i < vectorsArray.length; i++) {
+            rows[i] = new Vector(vectorsArray[i]);
+        }
+    }
+
+    public int getRowsCount() {
+        return rows.length;
+    }
+
+    public int getColumnsCount() {
+        return rows[0].getSize();
+    }
+
+    public Vector getRow(int index) {
+        validateRowIndex(index);
+
+        return rows[index];
+    }
+
+    public void setRow(Vector row, int index) {
+        validateRowIndex(index);
+        validateRowSize(row);
+
+        int rowSize = row.getSize();
+
+        for (int i = 0; i < rowSize; i++) {
+            rows[index].setComponent(i, row.getComponent(i));
+        }
+    }
+
+    public Vector getColumn(int index) {
+        validateColumnIndex(index);
+
+        int rowsCount = getRowsCount();
+        Vector column = new Vector(rowsCount);
+
+        for (int i = 0; i < rowsCount; i++) {
+            column.setComponent(i, rows[i].getComponent(index));
+        }
+
+        return column;
+    }
+
+    public void transpose() {
+        int columnsCount = getColumnsCount();
+
+        Matrix transposedMatrix = new Matrix(columnsCount, getRowsCount());
+
+        for (int i = 0; i < columnsCount; i++) {
+            transposedMatrix.rows[i] = getColumn(i);
+        }
+
+        rows = transposedMatrix.rows;
+    }
+
+    public void multiplyByScalar(double scalar) {
+        for (Vector row : rows) {
+            row.multiplyByScalar(scalar);
+        }
+    }
+
+    public void add(Matrix matrix) {
+        validateArithmeticOperationsPossibility(matrix);
+
+        int rowsCount = getRowsCount();
+
+        for (int i = 0; i < rowsCount; i++) {
+            rows[i].add(matrix.rows[i]);
+        }
+    }
+
+    public void subtract(Matrix matrix) {
+        validateArithmeticOperationsPossibility(matrix);
+
+        int rowsCount = getRowsCount();
+
+        for (int i = 0; i < rowsCount; i++) {
+            rows[i].subtract(matrix.rows[i]);
+        }
+    }
+
+    public double getDeterminant() {
+        validateSquareMatrix();
+        return 0;
+    }
+
+    private void validateColumnIndex(int index) {
+        if (index < 0 || index >= getColumnsCount()) {
+            throw new ArrayIndexOutOfBoundsException("The column index out of range.");
+        }
+    }
+
+    private void validateRowIndex(int index) {
+        if (index < 0 || index >= rows.length) {
+            throw new ArrayIndexOutOfBoundsException("The row index out of range.");
+        }
+    }
+
+    private void validateRowSize(Vector row) {
+        if (row.getSize() != getColumnsCount()) {
+            throw new IllegalArgumentException("The row size must match with the sizes of the matrix rows.");
+        }
+    }
+
+    private void validateSquareMatrix() {
+        if (getRowsCount() != getColumnsCount()) {
+            throw new IllegalArgumentException("To calculate the determinant, the matrix must be square.");
+        }
+    }
+
+    private void validateArithmeticOperationsPossibility(Matrix matrix) {
+        if (getRowsCount() != matrix.getRowsCount() || getColumnsCount() != matrix.getColumnsCount()) {
+            throw new IllegalArgumentException("For arithmetic operations, the matrices must be the same size.");
+        }
+    }
+
+    public static Matrix getSum(Matrix matrix1, Matrix matrix2) {
+        matrix1.validateArithmeticOperationsPossibility(matrix2);
+
+        int rowsCount = matrix1.getRowsCount();
+        Matrix sumMatrix = new Matrix(rowsCount, matrix1.getColumnsCount());
+
+        for (int i = 0; i < rowsCount; i++) {
+            sumMatrix.rows[i] = Vector.getSum(matrix1.rows[i], matrix2.rows[i]);
+        }
+
+        return sumMatrix;
+    }
+
+    public static Matrix getDifference(Matrix matrix1, Matrix matrix2) {
+        matrix1.validateArithmeticOperationsPossibility(matrix2);
+
+        int rowsCount = matrix1.getRowsCount();
+        Matrix differenceMatrix = new Matrix(rowsCount, matrix1.getColumnsCount());
+
+        for (int i = 0; i < rowsCount; i++) {
+            differenceMatrix.rows[i] = Vector.getDifference(matrix1.rows[i], matrix2.rows[i]);
+        }
+
+        return differenceMatrix;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("{");
+
+        for (Vector row : rows) {
+            stringBuilder.append(row.toString());
+            stringBuilder.append(", ");
+        }
+
+        stringBuilder.delete(stringBuilder.lastIndexOf(", "), stringBuilder.length());
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
+    }
 }
--- Vector/src/ru/academits/gerasimenko/vector/main_matrix/Main.java
+++ Vector/src/ru/academits/gerasimenko/vector/main_matrix/Main.java
@@ -1,2 +1,75 @@
-package ru.academits.gerasimenko.vector.main_matrix;public class Main {
+package ru.academits.gerasimenko.vector.main_matrix;
+
+import ru.academits.gerasimenko.vector.Matrix;
+import ru.academits.gerasimenko.vector.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Matrix matrix1 = new Matrix(4, 5);
+
+        System.out.println("1-st matrix: " + matrix1);
+        System.out.println("Rows count: " + matrix1.getRowsCount());
+        System.out.println("Columns count: " + matrix1.getColumnsCount());
+
+        System.out.println();
+
+        Matrix matrix2 = new Matrix(new double[][]{{1, 2, 3}, {2, 5, 7}, {6, 0, 1}, {4, 1, 7}});
+        System.out.println("2-nd matrix: " + matrix2);
+
+        Matrix matrix3 = new Matrix(matrix2);
+        System.out.println("3-rd matrix: " + matrix3);
+
+        Vector vector = new Vector(new double[]{17, 23, 42});
+        Vector vectorCopy = new Vector(vector);
+
+        Vector[] vectorsArray = new Vector[]{vectorCopy, vector, vectorCopy};
+
+        Matrix matrix4 = new Matrix(vectorsArray);
+        System.out.println("4-th matrix: " + matrix4);
+
+        System.out.println();
+
+        System.out.println("3-rd row from 2-nd matrix: " + matrix2.getRow(2));
+
+        matrix2.setRow(vector, 2);
+        System.out.println("2-nd matrix after setting the 3-rd row: " + matrix2);
+
+        System.out.println("3-rd column from 2-nd matrix: " + matrix2.getColumn(2));
+
+        System.out.println();
+
+        System.out.println("2-nd matrix: " + matrix2);
+
+        matrix2.transpose();
+        System.out.println("2-nd matrix after transposition: " + matrix2);
+
+        matrix2.transpose();
+        System.out.println("2-nd matrix after re-transposition: " + matrix2);
+
+        System.out.println();
+
+        matrix2.multiplyByScalar(3);
+        System.out.println("2-nd matrix after multiplication by scalar: " + matrix2);
+
+        System.out.println();
+
+        Matrix matrix5 = new Matrix(new double[][]{{2, 3, 4}, {-5, 6, -7}, {8, -9, 10}});
+        System.out.println("5-th matrix: " + matrix5);
+
+        Matrix matrix6 = new Matrix(new double[][]{{-1, 3, 5}, {5, 6, -3}, {1, -1, -1}});
+        System.out.println("6-th matrix: " + matrix6);
+
+        matrix5.add(matrix6);
+        System.out.println("5-th matrix after adding 6-th matrix: " + matrix5);
+
+        matrix6.subtract(matrix5);
+        System.out.println("6-th matrix after subtracting 5-th matrix: " + matrix6);
+
+        System.out.println();
+
+        System.out.println("5-th matrix now: " + matrix5);
+        System.out.println("6-th matrix now: " + matrix6);
+        System.out.println("Sum of the 5-th and 6-th matrices: " + Matrix.getSum(matrix5, matrix6));
+        System.out.println("Subtracting 6-th matrix from 5-th matrix: " + Matrix.getDifference(matrix5, matrix6));
+    }
 }
