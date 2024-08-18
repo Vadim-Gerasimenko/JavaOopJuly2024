19bd77814fc996c651ceca80023757867bd1e227
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-08-18 23:24:48 +0700

    refactor: add conversion method to controller

--- Temperature/src/ru/academits/gerasimenko/temperature/controller/Controller.java
+++ Temperature/src/ru/academits/gerasimenko/temperature/controller/Controller.java
@@ -1,5 +1,6 @@
 package ru.academits.gerasimenko.temperature.controller;
 
+import ru.academits.gerasimenko.temperature.constants.TemperatureScalesConstants;
 import ru.academits.gerasimenko.temperature.model.Converter;
 import ru.academits.gerasimenko.temperature.view.View;
 
@@ -10,6 +11,40 @@ public class Controller {
     public Controller(Converter converter, View view) {
         this.converter = converter;
         this.view = view;
+    }
+
+    public void convert(int inputScaleNumber, int outputScaleNumber, double temperature) {
+        switch (inputScaleNumber) {
+            case TemperatureScalesConstants.CELSIUS_SCALE_NUMBER -> {
+                if (outputScaleNumber == TemperatureScalesConstants.CELSIUS_SCALE_NUMBER) {
+                    view.showCelsiusTemperature(temperature);
+                } else if (outputScaleNumber == TemperatureScalesConstants.KELVIN_SCALE_NUMBER) {
+                    convertCelsiusToKelvin(temperature);
+                } else {
+                    convertCelsiusToFahrenheit(temperature);
+                }
+            }
+
+            case TemperatureScalesConstants.KELVIN_SCALE_NUMBER -> {
+                if (outputScaleNumber == TemperatureScalesConstants.CELSIUS_SCALE_NUMBER) {
+                    convertKelvinToCelsius(temperature);
+                } else if (outputScaleNumber == TemperatureScalesConstants.KELVIN_SCALE_NUMBER) {
+                    view.showKelvinTemperature(temperature);
+                } else {
+                    convertKelvinToFahrenheit(temperature);
+                }
+            }
+
+            case TemperatureScalesConstants.FAHRENHEIT_SCALE_NUMBER -> {
+                if (outputScaleNumber == TemperatureScalesConstants.CELSIUS_SCALE_NUMBER) {
+                    convertFahrenheitToCelsius(temperature);
+                } else if (outputScaleNumber == TemperatureScalesConstants.KELVIN_SCALE_NUMBER) {
+                    convertFahrenheitToKelvin(temperature);
+                } else {
+                    view.showFahrenheitTemperature(temperature);
+                }
+            }
+        }
     }
 
     public void convertCelsiusToKelvin(double celsiusTemperature) {
