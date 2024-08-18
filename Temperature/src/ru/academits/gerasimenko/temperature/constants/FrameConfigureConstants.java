2f54570bb4c30685a4272f943b3f1dc9d0f62a14
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-08-18 23:58:18 +0700

    chore: change frame default height

--- Temperature/src/ru/academits/gerasimenko/temperature/constants/FrameConfigureConstants.java
+++ Temperature/src/ru/academits/gerasimenko/temperature/constants/FrameConfigureConstants.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public final class FrameConfigureConstants {
     public static final int FRAME_DEFAULT_WIDTH = 400;
-    public static final int FRAME_DEFAULT_HEIGHT = 200;
+    public static final int FRAME_DEFAULT_HEIGHT = 150;
     public static final int DEFAULT_CLOSE_OPERATION = WindowConstants.EXIT_ON_CLOSE;
     public static final boolean IS_RESIZABLE_BY_DEFAULT = false;
     public static final boolean IS_VISIBLE_BY_DEFAULT = true;
