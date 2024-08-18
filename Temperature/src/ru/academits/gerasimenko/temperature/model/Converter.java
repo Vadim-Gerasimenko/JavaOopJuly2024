d95680fc8c074b3dc5ed932bb9a815a511a901b4
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-08-18 23:27:17 +0700

    refactor: remove unnecessary functionality converter interface

--- Temperature/src/ru/academits/gerasimenko/temperature/model/Converter.java
+++ Temperature/src/ru/academits/gerasimenko/temperature/model/Converter.java
@@ -1,11 +1,6 @@
 package ru.academits.gerasimenko.temperature.model;
 
 public interface Converter {
-    double FAHRENHEIT_ABSOLUTE_ZERO = -459.67;
-    double ICE_MELTING_KELVIN_TEMPERATURE = 273.15;
-    double ICE_MELTING_FAHRENHEIT_TEMPERATURE = 32;
-    double FAHRENHEIT_TO_CELSIUS_INVERSE_INCREMENTS_RATIO = 1.8;
-
     double convertCelsiusToKelvin(double celsiusTemperature);
 
     double convertCelsiusToFahrenheit(double celsiusTemperature);
