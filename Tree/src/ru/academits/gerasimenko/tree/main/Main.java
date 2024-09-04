e494a77f610908aaea2ca8de40de8c33323330dd
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-09-04 19:05:43 +0700

    test: test constructor with comparator and functional interface

--- Tree/src/ru/academits/gerasimenko/tree/main/Main.java
+++ Tree/src/ru/academits/gerasimenko/tree/main/Main.java
@@ -1,34 +1,41 @@
 package ru.academits.gerasimenko.tree.main;
 
+import ru.academits.gerasimenko.shapes.Rectangle;
 import ru.academits.gerasimenko.tree.Tree;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
         Tree<Integer> tree = new Tree<>();
-        List<Integer> elements = new LinkedList<>(List.of(8, 3, 10, 1, 6, 14, 4, 7, 13, 30, 25, 23, 24, 35, 27));
+        List<Integer> elements = new LinkedList<>(List.of(8, 3, 10, 1, 0, 6, 14, 4, 7, 13, 30, 25, 23, 24, 35, 27));
 
         for (Integer element : elements) {
             tree.insert(element);
         }
 
-        System.out.println("Tree nodes count: " + tree.getTreeNodesCount());
+        tree.insert(null);
+
+        System.out.println("Tree nodes count: " + tree.getSize());
         System.out.println();
 
-        Tree.breadthFirstSearch(tree);
-        System.out.println();
+        System.out.println("BFS and print nodes data:");
+        tree.breadthFirstSearch(integer -> System.out.print(integer + " "));
+        System.out.printf("%n%n");
 
-        Tree.depthFirstSearch(tree);
-        System.out.println();
+        System.out.println("DFS and print nodes data:");
+        tree.depthFirstSearch(integer -> System.out.print(integer + " "));
+        System.out.printf("%n%n");
 
         final int searchElement = 4;
 
-        if (tree.search(searchElement) == null) {
-            System.out.println("Search element '" + searchElement + "' not in the tree.");
-        } else {
+        if (tree.contains(searchElement)) {
             System.out.println("Search element '" + searchElement + "' in the tree.");
+        } else {
+            System.out.println("Search element '" + searchElement + "' not in the tree.");
         }
 
         final int removedElement = 14;
@@ -41,6 +48,21 @@ public class Main {
 
         System.out.println();
 
-        Tree.depthFirstSearchRecursive(tree);
+        Tree<Rectangle> rectanglesTree = new Tree<>(Comparator.comparingDouble(Rectangle::getArea));
+
+        rectanglesTree.insert(new Rectangle(10, 9));
+        rectanglesTree.insert(new Rectangle(5, 4));
+        rectanglesTree.insert(null);
+        rectanglesTree.insert(new Rectangle(2, 4));
+        rectanglesTree.insert(new Rectangle(50, 4));
+
+        AtomicInteger i = new AtomicInteger(1);
+
+        System.out.println("Recursive DFS and print info about rectangles:");
+        rectanglesTree.depthFirstSearchRecursively(rectangle -> {
+            System.out.println("Rectangle #" + i.getAndIncrement());
+            System.out.println(rectangle);
+            System.out.println();
+        });
     }
 }
