d299491fb879852525afa8fe10df7593c515e051
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-09-08 18:04:07 +0700

    fix: add person name check for null

--- Lambda/src/ru/academits/gerasimenko/lambda/Person.java
+++ Lambda/src/ru/academits/gerasimenko/lambda/Person.java
@@ -2,6 +2,10 @@ package ru.academits.gerasimenko.lambda;
 
 public record Person(String name, int age) {
     public Person {
+        if (name == null) {
+            throw new IllegalArgumentException("The 'name' field must not refer to null.");
+        }
+
         if (age < 0) {
             throw new IllegalArgumentException("The 'age' field must not be negative. "
                     + "Current age: " + age);
