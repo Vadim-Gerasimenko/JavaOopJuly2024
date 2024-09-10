df6185a1bdb6fa89dfd15dc94235525958ed2d7a
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-09-11 03:20:17 +0700

    refactor: implement the corrected model interface

--- Temperature/src/ru/academits/gerasimenko/temperature/model/TemperatureConverter.java
+++ Temperature/src/ru/academits/gerasimenko/temperature/model/TemperatureConverter.java
@@ -1,35 +1,10 @@
 package ru.academits.gerasimenko.temperature.model;
 
-import ru.academits.gerasimenko.temperature.constants.TemperatureScalesConstants;
+import ru.academits.gerasimenko.temperature.scales.Scale;
 
 public class TemperatureConverter implements Converter {
     @Override
-    public double convertCelsiusToKelvin(double celsiusTemperature) {
-        return celsiusTemperature + TemperatureScalesConstants.ICE_MELTING_KELVIN_TEMPERATURE;
-    }
-
-    @Override
-    public double convertCelsiusToFahrenheit(double celsiusTemperature) {
-        return celsiusTemperature * TemperatureScalesConstants.FAHRENHEIT_TO_CELSIUS_INVERSE_INCREMENTS_RATIO + TemperatureScalesConstants.ICE_MELTING_FAHRENHEIT_TEMPERATURE;
-    }
-
-    @Override
-    public double convertKelvinToCelsius(double kelvinTemperature) {
-        return kelvinTemperature - TemperatureScalesConstants.ICE_MELTING_KELVIN_TEMPERATURE;
-    }
-
-    @Override
-    public double convertKelvinToFahrenheit(double kelvinTemperature) {
-        return kelvinTemperature * TemperatureScalesConstants.FAHRENHEIT_TO_CELSIUS_INVERSE_INCREMENTS_RATIO + TemperatureScalesConstants.FAHRENHEIT_ABSOLUTE_ZERO;
-    }
-
-    @Override
-    public double convertFahrenheitToCelsius(double fahrenheitTemperature) {
-        return (fahrenheitTemperature - TemperatureScalesConstants.ICE_MELTING_FAHRENHEIT_TEMPERATURE) / TemperatureScalesConstants.FAHRENHEIT_TO_CELSIUS_INVERSE_INCREMENTS_RATIO;
-    }
-
-    @Override
-    public double convertFahrenheitToKelvin(double fahrenheitTemperature) {
-        return (fahrenheitTemperature - TemperatureScalesConstants.FAHRENHEIT_ABSOLUTE_ZERO) / TemperatureScalesConstants.FAHRENHEIT_TO_CELSIUS_INVERSE_INCREMENTS_RATIO;
+    public double convert(double temperature, Scale inputScale, Scale outputScale) {
+        return outputScale.convertFromCelsius(inputScale.convertToCelsius(temperature));
     }
 }
