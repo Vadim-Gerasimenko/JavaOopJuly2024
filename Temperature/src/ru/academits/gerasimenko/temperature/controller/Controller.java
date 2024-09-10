31023c8141a2a8ba55d7274f7c9a90bf2d7bc10f
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-09-11 03:24:18 +0700

    refactor: simplify the controller due to OCP

--- Temperature/src/ru/academits/gerasimenko/temperature/controller/Controller.java
+++ Temperature/src/ru/academits/gerasimenko/temperature/controller/Controller.java
@@ -1,7 +1,7 @@
 package ru.academits.gerasimenko.temperature.controller;
 
-import ru.academits.gerasimenko.temperature.constants.TemperatureScalesConstants;
 import ru.academits.gerasimenko.temperature.model.Converter;
+import ru.academits.gerasimenko.temperature.scales.Scale;
 import ru.academits.gerasimenko.temperature.view.View;
 
 public class Controller {
@@ -13,67 +13,8 @@ public class Controller {
         this.view = view;
     }
 
-    public void convert(int inputScaleNumber, int outputScaleNumber, double temperature) {
-        switch (inputScaleNumber) {
-            case TemperatureScalesConstants.CELSIUS_SCALE_NUMBER -> {
-                if (outputScaleNumber == TemperatureScalesConstants.CELSIUS_SCALE_NUMBER) {
-                    view.showCelsiusTemperature(temperature);
-                } else if (outputScaleNumber == TemperatureScalesConstants.KELVIN_SCALE_NUMBER) {
-                    convertCelsiusToKelvin(temperature);
-                } else {
-                    convertCelsiusToFahrenheit(temperature);
-                }
-            }
-
-            case TemperatureScalesConstants.KELVIN_SCALE_NUMBER -> {
-                if (outputScaleNumber == TemperatureScalesConstants.CELSIUS_SCALE_NUMBER) {
-                    convertKelvinToCelsius(temperature);
-                } else if (outputScaleNumber == TemperatureScalesConstants.KELVIN_SCALE_NUMBER) {
-                    view.showKelvinTemperature(temperature);
-                } else {
-                    convertKelvinToFahrenheit(temperature);
-                }
-            }
-
-            case TemperatureScalesConstants.FAHRENHEIT_SCALE_NUMBER -> {
-                if (outputScaleNumber == TemperatureScalesConstants.CELSIUS_SCALE_NUMBER) {
-                    convertFahrenheitToCelsius(temperature);
-                } else if (outputScaleNumber == TemperatureScalesConstants.KELVIN_SCALE_NUMBER) {
-                    convertFahrenheitToKelvin(temperature);
-                } else {
-                    view.showFahrenheitTemperature(temperature);
-                }
-            }
-        }
-    }
-
-    public void convertCelsiusToKelvin(double celsiusTemperature) {
-        double kelvinTemperature = converter.convertCelsiusToKelvin(celsiusTemperature);
-        view.showKelvinTemperature(kelvinTemperature);
-    }
-
-    public void convertCelsiusToFahrenheit(double celsiusTemperature) {
-        double fahrenheitTemperature = converter.convertCelsiusToFahrenheit(celsiusTemperature);
-        view.showFahrenheitTemperature(fahrenheitTemperature);
-    }
-
-    public void convertKelvinToCelsius(double kelvinTemperature) {
-        double celsiusTemperature = converter.convertKelvinToCelsius(kelvinTemperature);
-        view.showCelsiusTemperature(celsiusTemperature);
-    }
-
-    public void convertKelvinToFahrenheit(double kelvinTemperature) {
-        double fahrenheitTemperature = converter.convertKelvinToFahrenheit(kelvinTemperature);
-        view.showFahrenheitTemperature(fahrenheitTemperature);
-    }
-
-    public void convertFahrenheitToCelsius(double fahrenheitTemperature) {
-        double celsiusTemperature = converter.convertFahrenheitToCelsius(fahrenheitTemperature);
-        view.showCelsiusTemperature(celsiusTemperature);
-    }
-
-    public void convertFahrenheitToKelvin(double fahrenheitTemperature) {
-        double kelvinTemperature = converter.convertFahrenheitToKelvin(fahrenheitTemperature);
-        view.showKelvinTemperature(kelvinTemperature);
+    public void convert(double temperature, Scale inputScale, Scale outputScale) {
+        double convertedTemperature = converter.convert(temperature, inputScale, outputScale);
+        view.showTemperature(convertedTemperature);
     }
 }
