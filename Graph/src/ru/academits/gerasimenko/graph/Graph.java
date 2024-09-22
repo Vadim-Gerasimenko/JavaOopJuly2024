9ad3822a98dc949a463e333d2e4498069d9f5960
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-09-22 16:09:01 +0700

    fix: give search methods the ability to accept Consumer as argument

--- Graph/src/ru/academits/gerasimenko/graph/Graph.java
+++ Graph/src/ru/academits/gerasimenko/graph/Graph.java
@@ -3,97 +3,93 @@ package ru.academits.gerasimenko.graph;
 import java.util.Deque;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.function.Consumer;
 
 public class Graph {
-    private final boolean[][] graph;
+    private final int[][] adjacencyMatrix;
 
-    public Graph(boolean[][] graph) {
-        for (boolean[] component : graph) {
-            if (graph.length != component.length) {
+    public Graph(int[][] adjacencyMatrix) {
+        for (int[] component : adjacencyMatrix) {
+            if (adjacencyMatrix.length != component.length) {
                 throw new IllegalArgumentException("the adjacency matrix must be square.");
             }
         }
 
-        this.graph = graph;
+        this.adjacencyMatrix = adjacencyMatrix;
     }
 
-    public static void breadthFirstSearch(Graph graph) {
-        final int verticesCount = graph.graph.length;
+    public void searchBreadthFirst(Consumer<Integer> consumer) {
+        int verticesCount = adjacencyMatrix.length;
         boolean[] visited = new boolean[verticesCount];
         Queue<Integer> queue = new LinkedList<>();
 
-        System.out.println("BFS and print vertices:");
-
         for (int i = 0; i < verticesCount; ++i) {
-            queue.add(i);
+            if (!visited[i]) {
+                queue.add(i);
 
-            while (!queue.isEmpty()) {
-                int vertex = queue.remove();
+                while (!queue.isEmpty()) {
+                    int vertex = queue.remove();
 
-                if (!visited[vertex]) {
-                    System.out.print(vertex + " ");
+                    if (!visited[vertex]) {
+                        consumer.accept(vertex);
+                        visited[vertex] = true;
 
-                    for (int j = 0; j < verticesCount; ++j) {
-                        if (vertex != j && graph.graph[vertex][j] && !visited[j]) {
-                            queue.add(j);
+                        for (int j = 0; j < verticesCount; ++j) {
+                            if (vertex != j && adjacencyMatrix[vertex][j] != 0 && !visited[j]) {
+                                queue.add(j);
+                            }
                         }
                     }
-
-                    visited[vertex] = true;
                 }
             }
         }
-
-        System.out.println();
     }
 
-
-    public static void depthFirstSearch(Graph graph) {
-        final int verticesCount = graph.graph.length;
+    public void searchDepthFirst(Consumer<Integer> consumer) {
+        int verticesCount = adjacencyMatrix.length;
         boolean[] visited = new boolean[verticesCount];
         Deque<Integer> stack = new LinkedList<>();
 
-        System.out.println("DFS and print vertices:");
-
         for (int i = 0; i < verticesCount; ++i) {
-            stack.addFirst(i);
+            if (!visited[i]) {
+                stack.push(i);
 
-            while (!stack.isEmpty()) {
-                int vertex = stack.removeFirst();
+                while (!stack.isEmpty()) {
+                    int vertex = stack.pop();
 
-                if (!visited[vertex]) {
-                    System.out.print(vertex + " ");
+                    if (!visited[vertex]) {
+                        consumer.accept(vertex);
+                        visited[vertex] = true;
 
-                    for (int j = verticesCount - 1; j != 0; --j) {
-                        if (vertex != j && graph.graph[vertex][j] && !visited[j]) {
-                            stack.addFirst(j);
+                        for (int j = verticesCount - 1; j != 0; --j) {
+                            if (vertex != j && adjacencyMatrix[vertex][j] != 0 && !visited[j]) {
+                                stack.push(j);
+                            }
                         }
                     }
-
-                    visited[vertex] = true;
                 }
             }
         }
-
-        System.out.println();
     }
 
-    public static void depthFirstSearchRecursive(Graph graph) {
-        final int verticesCount = graph.graph.length;
+    public void searchDepthFirstRecursively(Consumer<Integer> consumer) {
+        int verticesCount = adjacencyMatrix.length;
         boolean[] visited = new boolean[verticesCount];
 
-        System.out.println("Recursive DFS and print vertices:");
-        visit(graph, visited, 0);
-        System.out.println();
+        for (int i = 0; i < verticesCount; ++i) {
+            if (!visited[i]) {
+                visitVertexWithSearchDepthFirstRecursively(visited, i, consumer);
+            }
+        }
     }
 
-    private static void visit(Graph graph, boolean[] visited, int vertex) {
+    private void visitVertexWithSearchDepthFirstRecursively(boolean[] visited, int vertex, Consumer<Integer> consumer) {
+        consumer.accept(vertex);
         visited[vertex] = true;
-        System.out.print(vertex + " ");
 
         for (int i = 0; i < visited.length; ++i) {
-            if (vertex != i && graph.graph[vertex][i] && !visited[i]) {
-                visit(graph, visited, i);
+            if (vertex != i && adjacencyMatrix[vertex][i] != 0 && !visited[i]) {
+                visitVertexWithSearchDepthFirstRecursively(visited, i, consumer);
             }
         }
     }
