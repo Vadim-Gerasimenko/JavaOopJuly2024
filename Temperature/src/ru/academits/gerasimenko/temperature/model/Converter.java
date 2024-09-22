3577d8f661ac5337675c8b3126835192541f2a90
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-09-22 16:48:38 +0700

    fix: implement the ability to receive a scales list from the model

--- Temperature/src/ru/academits/gerasimenko/temperature/model/Converter.java
+++ Temperature/src/ru/academits/gerasimenko/temperature/model/Converter.java
@@ -2,6 +2,10 @@ package ru.academits.gerasimenko.temperature.model;
 
 import ru.academits.gerasimenko.temperature.scales.Scale;
 
+import java.util.List;
+
 public interface Converter {
     double convert(double temperature, Scale inputScale, Scale outputScale);
+
+    List<Scale> getAvailableScalesList();
 }
--- Temperature/src/ru/academits/gerasimenko/temperature/model/TemperatureConverter.java
+++ Temperature/src/ru/academits/gerasimenko/temperature/model/TemperatureConverter.java
@@ -1,10 +1,26 @@
 package ru.academits.gerasimenko.temperature.model;
 
+import ru.academits.gerasimenko.temperature.scales.CelsiusScale;
+import ru.academits.gerasimenko.temperature.scales.FahrenheitScale;
+import ru.academits.gerasimenko.temperature.scales.KelvinScale;
 import ru.academits.gerasimenko.temperature.scales.Scale;
 
+import java.util.List;
+
 public class TemperatureConverter implements Converter {
+    private final List<Scale> scalesList = List.of(
+            new CelsiusScale(),
+            new KelvinScale(),
+            new FahrenheitScale()
+    );
+
     @Override
     public double convert(double temperature, Scale inputScale, Scale outputScale) {
         return outputScale.convertFromCelsius(inputScale.convertToCelsius(temperature));
+    }
+
+    @Override
+    public List<Scale> getAvailableScalesList() {
+        return scalesList;
     }
 }
