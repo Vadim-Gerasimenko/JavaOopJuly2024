97fb9dcf8b2cd49a333399a9420b9d5efd5a310d
Vadim Gerasimenko <vadim2016gerasimenko@gmail.com>
2024-09-29 19:17:38 +0700

    fix: pass the available scales list to the model constructor

--- Temperature/src/ru/academits/gerasimenko/temperature/Main.java
+++ Temperature/src/ru/academits/gerasimenko/temperature/Main.java
@@ -2,11 +2,20 @@ package ru.academits.gerasimenko.temperature;
 
 import ru.academits.gerasimenko.temperature.controller.*;
 import ru.academits.gerasimenko.temperature.model.*;
+import ru.academits.gerasimenko.temperature.scales.CelsiusScale;
+import ru.academits.gerasimenko.temperature.scales.FahrenheitScale;
+import ru.academits.gerasimenko.temperature.scales.KelvinScale;
 import ru.academits.gerasimenko.temperature.view.*;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Converter converter = new TemperatureConverter();
+        Converter converter = new TemperatureConverter(List.of(
+                new CelsiusScale(),
+                new FahrenheitScale(),
+                new KelvinScale()
+        ));
         View view = new DesktopView();
 
         Controller controller = new Controller(converter, view);
